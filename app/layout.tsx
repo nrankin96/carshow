c6df06a591acd55d302f4ad41ce1067ce6004dba
Nykia <nykiarankin96@gmail.com>
2023-10-13 09:57:40 -0400

    add Searchbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import Header from '@/components/Header'
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 import './globals.css'
 
 
 
-export const metadata: Metadata = {
+export const metadata ={
   title: 'Car Hub',
   description: "Discover world's best car showcase application",
 }
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className='relative' suppressHydrationWarning={true}>
         <Header />
         {children}
+        <Footer />
         </body>
     </html>
   )
